b37b1c6d9d2af5e9dbdbb2cc51fe7ae8dfe51d23
Alexander <ab@digital-benefits.de>
2026-08-05 09:47:57 +0000

    fix(nvim): limit system clipboard to y/p, leave d/c/x untouched

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,11 @@
--- Systemzwischenablage ("+) als Standard-Register: y/p arbeiten direkt damit
-vim.opt.clipboard = "unnamedplus"
-
 -- Zeilennummern anzeigen
 vim.opt.number = true
 
 -- Farbschema
 vim.cmd.colorscheme("slate")
+
+-- Nur y/p auf Systemzwischenablage ("+), d/c/x bleiben unangetastet
+vim.keymap.set({ "n", "x" }, "y", '"+y')
+vim.keymap.set("n", "Y", '"+y$')
+vim.keymap.set("n", "p", '"+p')
+vim.keymap.set("n", "P", '"+P')
